760c97ba00f61c7a7be6ee45122f4d93f5454515
Thomas <71586988+Tommypop2@users.noreply.github.com>
2023-06-12 16:15:38 +0100

    Made more methods private, so that only get and set are exposed publically

--- cpp_reactivity/main.cpp
+++ cpp_reactivity/main.cpp
@@ -2,7 +2,6 @@
 #include <vector>
 #include <functional>
 #include <set>
-using namespace std;
 void *current_memo = NULL;
 template <typename T>
 class Memo
@@ -11,10 +10,10 @@ public:
     T value;
     std::set<Memo *> subscribers;
     std::set<Memo *> dependencies;
-    function<T()> computation_ptr = NULL;
+    std::function<T()> computation_ptr = NULL;
     bool dirty = false;
     int depsDirtyCount = 0;
-    Memo(function<T()> computation_ptr)
+    Memo(std::function<T()> computation_ptr)
     {
         this->computation_ptr = computation_ptr;
         this->update();
@@ -31,6 +30,23 @@ public:
             sub->dependencies.erase(this);
         }
     }
+    get()
+    {
+        this->track();
+        return this->value;
+    }
+    set(int newValue)
+    {
+        if (this->value == newValue)
+        {
+            return 0;
+        }
+        this->value = newValue;
+        this->notifySubscribers();
+        return 0;
+    }
+
+private:
     update()
     {
         void *prevMemo = current_memo;
@@ -57,23 +73,6 @@ public:
         currentMemoPtr->dependencies.insert(this);
         return 0;
     }
-    get()
-    {
-        this->track();
-        return this->value;
-    }
-    set(int newValue)
-    {
-        if (this->value == newValue)
-        {
-            return 0;
-        }
-        this->value = newValue;
-        this->notifySubscribers();
-        return 0;
-    }
-
-private:
     increment()
     {
         this->depsDirtyCount++;
@@ -123,17 +122,17 @@ int main()
     Memo<int> *A = new Memo<int>([]()
                                  { return 0; });
     Memo<int> *B = new Memo<int>([&A]()
-                                 { cout << "B updating" << endl;return A->get() + 1; });
+                                 { std::cout << "B updating" << std::endl;return A->get() + 1; });
     Memo<int> *C = new Memo<int>([&A]()
-                                 { cout << "C updating" << endl;return A->get() + 2; });
+                                 { std::cout << "C updating" << std::endl;return A->get() + 2; });
     Memo<int> *D = new Memo<int>([&B, &C]()
                                  { return B->get() + C->get(); });
     Memo<int> *E = new Memo<int>([&D]()
                                  { return D->get() + 5; });
-    cout << E->get() << endl;
+    std::cout << E->get() << std::endl;
     A->set(2);
-    cout << E->get() << endl;
+    std::cout << E->get() << std::endl;
     A->set(3);
-    cout << E->get() << endl;
+    std::cout << E->get() << std::endl;
     return 0;
 }
